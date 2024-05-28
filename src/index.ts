fab8e0db536ca6aecfc3ea18e41f9a024ec1179d
Brian Richardson <brichardson@matterport.com>
2024-05-28 10:04:09 -0700

    Marbling is working!

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,86 @@
 // DONE: Draw one drop
 // DONE: Earcut
 // DONE: Draw NxN drops
-
+// DONE: Trigger drops with mouse
+// DONE: Retriangulate
+// DONE: Different colors per drop
+// DONE: Cleanup
+// DONE: https://people.csail.mit.edu/jaffer/Marbling/Dropping-Paint
+// https://tchayen.github.io/posts/triangulation-of-polygons
 import * as earcut from "earcut";
 
 // Draw NxN drops with different colors
 export {};
 
+//
+// Drop simulation
+//
+const NUM_DROP_VERTICES = 100;
+const NUM_DROPS = 256;
+const TRIANGLES_GENERATED = (NUM_DROP_VERTICES - 2);
+
+const vertices = new Float32Array(NUM_DROPS * NUM_DROP_VERTICES * 2);
+const indices = new Uint32Array(NUM_DROPS * TRIANGLES_GENERATED * 2 * 3);
+const colors = new Float32Array((NUM_DROPS + 1) * 4);
+
+let currentDrop = 0;
+
+function makeDrop(dropIndex: number, x: number, y: number, radius: number, r: number = 1, g: number = 0, b: number = 0): void {
+  let idx = dropIndex * NUM_DROP_VERTICES * 2;
+  for (let i = 0; i < NUM_DROP_VERTICES; i++) {
+    const angle = i * ((Math.PI * 2) / NUM_DROP_VERTICES);
+    vertices[idx++] = Math.cos(angle) * radius + x;
+    vertices[idx++] = Math.sin(angle) * radius + y;
+  }  
+  let colorIndex = dropIndex * 4;
+  colors[colorIndex++] = r;
+  colors[colorIndex++] = g;
+  colors[colorIndex++] = b;
+  colors[colorIndex++] = 1.0;
+}
+
+function triangulateDrop(dropIndex: number) {
+  const verts = vertices.slice(dropIndex * NUM_DROP_VERTICES * 2, (dropIndex+1) * NUM_DROP_VERTICES * 2);
+  const tris = earcut(verts);
+  const offset = dropIndex * NUM_DROP_VERTICES;
+  indices.set(tris.map(i => i + offset), TRIANGLES_GENERATED * 3 * 2 * dropIndex);
+}
+
+for (let i = 0; i < NUM_DROPS; i++) {
+  makeDrop(i, 0, 0, 0);
+  triangulateDrop(i);
+}
+
+function handleClick(x: number, y: number): void {
+  const radius = 0.15;
+  simulateDrop(x, y, radius);
+  makeDrop(currentDrop, x, y, radius, Math.random(), Math.random(), Math.random());
+  for (let i = 0; i < NUM_DROPS; i++) {
+    triangulateDrop(i);
+  }
+  currentDrop++;
+  if (currentDrop >= NUM_DROPS) {
+    currentDrop = 0;
+  }
+  colors[NUM_DROPS * 4] = currentDrop;
+}
+
+function simulateDrop(cx: number, cy: number, r: number): void {
+  const r2 = r*r;
+  let idx = 0;
+  for (let i = 0; i < NUM_DROPS * NUM_DROP_VERTICES; i++) {
+    const x = vertices[idx];
+    const y = vertices[idx+1];
+    const pMinusC = (x-cx)*(x-cx)+(y-cy)*(y-cy);
+    const lastTerm = Math.sqrt(1 + r2 / pMinusC);
+    vertices[idx++] = cx + (x - cx) * lastTerm;
+    vertices[idx++] = cy + (y - cy) * lastTerm;
+  }
+}
+
+//
+// WebGPU stuff
+// 
 if (!navigator.gpu) {
   throw new Error('WebGPU not supported!');
 }
@@ -21,31 +95,22 @@ if (!device) {
   throw new Error('Unable to get device!');
 }
 
-const context = document.getElementsByTagName('canvas')[0].getContext('webgpu');
+const canvas = document.getElementsByTagName('canvas')[0];
+canvas.onclick = (ev: MouseEvent) => {
+  const x = (ev.clientX / canvas.width) * 2.0 - 1.0;
+  const y = -((ev.clientY / canvas.height) * 2.0 - 1.0);
+  handleClick(x ,y);
+  device.queue.writeBuffer(vertexBuffer, 0, vertices);
+  device.queue.writeBuffer(indexBuffer, 0, indices);
+  device.queue.writeBuffer(uniformBuffer, 0, colors);
+  draw();
+}
+const context = canvas.getContext('webgpu');
 const format = navigator.gpu.getPreferredCanvasFormat();
 context.configure({
   device,
   format
 });
-
-const NUM_DROP_VERTICES = 20;
-const NUM_DROPS = 4;
-
-const vertices = new Float32Array(NUM_DROPS * NUM_DROP_VERTICES * 2);
-
-function makeDrop(dropIndex: number, x: number, y: number, radius: number): void {
-  let idx = dropIndex * NUM_DROP_VERTICES * 2;
-  for (let i = 0; i < NUM_DROP_VERTICES; i++) {
-    const angle = i * ((Math.PI * 2) / NUM_DROP_VERTICES);
-    vertices[idx++] = Math.cos(angle) * radius + x;
-    vertices[idx++] = Math.sin(angle) * radius + y;
-  }  
-}
-
-makeDrop(0, -0.5, -0.5, 0.25);
-makeDrop(1, 0.5, -0.5, 0.25);
-makeDrop(2, -0.5, 0.5, 0.25);
-makeDrop(3, 0.5, 0.5, 0.25);
 
 const vertexBuffer = device.createBuffer({
   label: 'drop vertices',
@@ -62,14 +127,6 @@ const vertexBufferLayout: GPUVertexBufferLayout = {
       }]
 };
 
-const triangles = [];
-for (let i = 0; i < NUM_DROPS; i++) {
-  const verts = vertices.slice(i * NUM_DROP_VERTICES * 2, (i+1) * NUM_DROP_VERTICES * 2);
-  const tris = earcut(verts);
-  const offset = i * NUM_DROP_VERTICES;
-  triangles.push(...tris.map(i => i + offset));
-}
-const indices = new Uint32Array(triangles);
 const indexBuffer = device.createBuffer({
   label: 'drop indices',
   size: indices.byteLength,
@@ -77,26 +134,71 @@ const indexBuffer = device.createBuffer({
  });
 device.queue.writeBuffer(indexBuffer, 0, indices);
 
+const uniformBuffer = device.createBuffer({
+  label: 'drop colors',
+  size: colors.byteLength,
+  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+});
+device.queue.writeBuffer(uniformBuffer, 0, colors);
+
 const shaderModule = device.createShaderModule({
   label: 'drop shader',
   code: `
+    struct VertexOutput {
+      @builtin(position) pos: vec4f,
+      @location(0) color: vec3f,
+    }
+
+    struct DropUniforms {
+      colors: array<vec3f, ${NUM_DROPS}>,
+      currentDrop: f32,
+    }
+
+    @group(0) @binding(0) var<uniform> drops: DropUniforms;
+
     @vertex
     fn vertexMain(
+      @builtin(vertex_index) vertIndex: u32,
       @location(0) pos: vec2f
-    ) -> @builtin(position) vec4f {
-      return vec4f(pos, 0, 1);
+    ) -> VertexOutput {
+      var output: VertexOutput;
+      let dropIndex = vertIndex / ${NUM_DROP_VERTICES};
+      var z = f32(dropIndex) - drops.currentDrop;
+      if (z < 0) {
+        z += ${NUM_DROPS};
+      }
+      output.pos = vec4f(pos, (1.0 - (z / ${NUM_DROPS})) * 0.99, 1);
+      output.color = drops.colors[vertIndex / ${NUM_DROP_VERTICES}];
+      return output;
     }
 
     @fragment
-    fn fragmentMain() -> @location(0) vec4f {
-      return vec4f(1, 0, 0, 1);
+    fn fragmentMain(input: VertexOutput) -> @location(0) vec4f {
+      return vec4f(input.color, 1);
     }
   `
 });
 
+const bindGroupLayout = device.createBindGroupLayout({
+  label: 'drop bind group layout',
+  entries: [
+    {
+      binding: 0,
+      visibility: GPUShaderStage.VERTEX,
+      buffer: {}
+    }
+  ]
+});
+
+const depthTexture = device.createTexture({
+  size: [512, 512],
+  format: 'depth24plus',
+  usage: GPUTextureUsage.RENDER_ATTACHMENT,
+});
+
 const dropPipeline = device.createRenderPipeline({
   label: 'drop pipeline',
-  layout: 'auto',
+  layout: device.createPipelineLayout({ bindGroupLayouts: [bindGroupLayout]}),
   vertex: {
     module: shaderModule,
     entryPoint: 'vertexMain',
@@ -108,21 +210,50 @@ const dropPipeline = device.createRenderPipeline({
     targets: [{
       format,
     }]
+  },
+  depthStencil: {
+    depthWriteEnabled: true,
+    depthCompare: 'less',
+    format: 'depth24plus',
   }
 });
 
-const encoder = device.createCommandEncoder();
-const pass = encoder.beginRenderPass({
-  colorAttachments: [{
-    view: context.getCurrentTexture().createView(),
-    loadOp: "clear",
-    clearValue: { r: 0, g: 0, b: 0.6, a: 1 },
-    storeOp: 'store',
-  }]});
-pass.setPipeline(dropPipeline);
-pass.setVertexBuffer(0, vertexBuffer);
-pass.setIndexBuffer(indexBuffer, 'uint32');
-pass.drawIndexed(indices.length);
-pass.end();
-const commandBuffer = encoder.finish();
-device.queue.submit([commandBuffer]);
+const bindGroup = device.createBindGroup({
+  label: 'Drop bind group',
+  layout: bindGroupLayout,
+  entries: [
+    {
+      binding: 0,
+      resource: { buffer: uniformBuffer }
+    }
+  ]
+});
+
+function draw() {
+  const encoder = device.createCommandEncoder();
+  const pass = encoder.beginRenderPass({
+    colorAttachments: 
+      [{
+        view: context.getCurrentTexture().createView(),
+        loadOp: "clear",
+        clearValue: { r: 0, g: 0, b: 0.6, a: 1 },
+        storeOp: 'store',
+      }],
+    depthStencilAttachment: {
+      view: depthTexture.createView(),
+      depthClearValue: 1.0,
+      depthLoadOp: 'clear',
+      depthStoreOp: 'store',
+    }
+  });
+  pass.setPipeline(dropPipeline);
+  pass.setVertexBuffer(0, vertexBuffer);
+  pass.setIndexBuffer(indexBuffer, 'uint32');
+  pass.setBindGroup(0, bindGroup);
+  pass.drawIndexed(indices.length);
+  pass.end();
+  const commandBuffer = encoder.finish();
+  device.queue.submit([commandBuffer]);  
+}
+
+draw();
